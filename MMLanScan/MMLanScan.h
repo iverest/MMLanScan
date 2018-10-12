77a3f033d2953f14acb7d39e495ddff8ec7a867f
Boyan Krastev <bkrastev@iverest.com>
2018-10-12 10:17:12 +0300

    Add modulemap

---
+++ MMLanScan/MMLanScan.h
@@ -0,0 +1,20 @@
+//
+//  MMLanScan.h
+//  MMLanScan
+//
+//  Created by Boyan Krastev on 12.10.18.
+//  Copyright © 2018 Miksoft. All rights reserved.
+//
+
+#ifndef MMLanScan_h
+#define MMLanScan_h
+
+#import "MMLANScanner.h"
+#import "LANProperties.h"
+#import "PingOperation.h"
+#import "MMLANScanner.h"
+#import "MACOperation.h"
+#import "MacFinder.h"
+#import "MMDevice.h"
+
+#endif /* MMLanScan_h */
